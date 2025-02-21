abef2cd13d2f28b3e50585a1af3d04e5190cc6c0
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-21 11:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-function volume_sphere(event) {
-    //Write your code here
-  event.preventDefault();
-	const radiusInput = document.getElementById('radius').value;
-	const radius = parseFloat(radiusInput);
-	if (isNaN(radius) || radius < 0){
-		document.getElementById('volume').value = 'NaN';
-		return;
-	}
-	const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-	const roundedvolume = volume.toFixed(4);
-	document.getElementById('volume') = roundedvolume;
-} 
-window.onload = function()
-	{
-		document.getElementById('MyForm').onsubmit = volume_sphere;
-	};
+function volume_sphere() {
+    // Retrieve the radius value entered by the user
+    var radius = document.getElementById('radius').value;
+    if (isNaN(radius) || radius < 0) {
+        document.getElementById('volume').value = 'NaN';
+        return;
+    var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.getElementById('volume').value = volume.toFixed(4);
+}
+};
 
