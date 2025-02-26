510b2c7cde5b1e7baeb4221003ddfa16f2d6d05c
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-26 10:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function volume_sphere() {
     // Retrieve the radius value entered by the user
     var radius = document.getElementById('radius').value;
+	radius = parseFloat(radius);
     if (isNaN(radius) || radius < 0) {
         document.getElementById('volume').value = 'NaN';
         return;
+	}
     var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
     document.getElementById('volume').value = volume.toFixed(4);
 }
-};
 
